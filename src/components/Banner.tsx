c8578311eb4ca37c150e44162ff2892e38bbf00f
intaniger <intaniger@gmail.com>
2023-11-27 02:43:43 +0700

    ci: :construction_worker: added workflow script along with version label and front page

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,11 @@
+import { VERSION } from '../const'
+
 const Banner = () => {
   return (
     <div className="flex flex-col gap-y-3 px-32 py-24 bg-orange-200/50">
       <p className="font-bold text-5xl text-orange-500">LearnHub</p>
       <p className="text-gray-500">Hub for Educational Videos</p>
+      <p className="text-sm text-gray-500">v.{VERSION}</p>
     </div>
   )
 }
